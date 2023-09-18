f095fc3123619ceb09af846bd7f1dcd67ce4a42b
chrisnel01 <chrisnel1011@gmail.com>
2023-09-17 23:34:46 -0400

    Auto commit from scheduler

--- scheduler.py
+++ scheduler.py
@@ -17,7 +17,7 @@ def run_updater():
         print(f"Error: {e}")
 
 # Schedule the updater to run at 3:00 AM every day
-schedule.every().day.at("23:21").do(run_updater)
+schedule.every().day.at("23:34").do(run_updater)
 
 while True:
     schedule.run_pending()
--- webpage.py
+++ webpage.py
@@ -1,17 +1,18 @@
 from flask import Flask, render_template
-
 import json
+import time
 
 app = Flask(__name__)
 
 with open("rosters.json", "r") as json_file:
     data = json.load(json_file)
 
+# Record the time when the Flask application starts
+last_run_time = time.strftime("%Y-%m-%d %H:%M:%S")
 
 @app.route('/')
 def index():
-    return render_template('index.html', data=data)
-
+    return render_template('index.html', data=data, last_run_time=last_run_time)
 
 if __name__ == '__main__':
     app.run(debug=True)
