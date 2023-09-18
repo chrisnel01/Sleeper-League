4376b0721edc41f37253d6cc920fcaa89fa30c2b
chrisnel01 <chrisnel1011@gmail.com>
2023-09-18 12:11:24 -0400

    Auto commit from scheduler

--- scheduler.py
+++ scheduler.py
@@ -1,8 +1,9 @@
 import schedule
 import time
 import subprocess
-
+from datetime import datetime
 def run_updater():
+    log_data = []
     try:
         # Run your updater script
         subprocess.run(["python", "updater.py"], check=True)
@@ -13,11 +14,15 @@ def run_updater():
         subprocess.run(["git", "commit", "-m", "Auto commit from scheduler"], check=True)
         subprocess.run(["git", "push"], check=True)
         print("Git changes have been committed and pushed to the repository.")
+        
+        now = datetime.now()
+        log_data.append({"timestamp": now.strftime("%Y-%m-%d %H:%M:%S")})
+        print("Updated: ", log_data)
     except subprocess.CalledProcessError as e:
         print(f"Error: {e}")
 
 # Schedule the updater to run at 3:00 AM every day
-schedule.every().day.at("00:05").do(run_updater)
+schedule.every().day.at("12:11").do(run_updater)
 
 while True:
     schedule.run_pending()
--- webpage.py
+++ webpage.py
@@ -7,14 +7,10 @@ app = Flask(__name__)
 with open("rosters.json", "r") as json_file:
     data = json.load(json_file)
 
-# Record the time when the Flask application starts
-current_time = datetime.now()
-adjusted_time = current_time - timedelta(hours=4)  # Subtract 4 hours
-last_run_time = adjusted_time.strftime("%Y-%m-%d %H:%M:%S")
 
 @app.route('/')
 def index():
-    return render_template('index.html', data=data, last_run_time=last_run_time)
+    return render_template('index.html', data=data)
 
 if __name__ == '__main__':
     app.run(debug=True)
